dbf22c42637ddcb153e6d27e4adf8dc521b9e7c9
ChimGoKien <38057360+quocle108@users.noreply.github.com>
2020-03-23 17:36:34 +0700

    release v1.0.0 (#4)
    * release v1.0.0

--- README.md
+++ README.md
@@ -1,89 +1,102 @@
-# cryptobadge
+Crypto-Badge
+----------
 
-## Version : 1.0.0
+Version 1.0.0
 
-Folder structure
-
-- `src` directory, the `cpp` files are there.
-- `include` directory, the `hpp` files are there.
-- `test` directory, test scripts are there.
-- `ricardian` contains the Ricardian contract files.
-- `.vscode` directory that contains *IntelliSense* definitions, task 
-  definitions, etc.
-- `test` directory, buil file and script.
-- `example` directory, an example to deploy/run smart contract.
-- `CMakeLists.txt` file, CMake definitions.
+This contract provides multiple functionalities:
+- Users can register to become a badge issuer,
+- Issuer can create/update badge and assign to badge owner
+- Badge owner can create new certification based on the created badge
+- Badge owner can create new certification and issue to users (issuer pay for RAM) directly or offer it to users (Users must pay RAM if they accept the offer)
+- Badge owner can cancel the offered certification.
+- User can accept the offer to receive certification from Badge owner
+- User can choose to public certification detail
 
 Dependencies:
-* [eosio v1.8.x](https://github.com/EOSIO/eos/releases/tag/v1.8.0)
-* [eosio.cdt v1.6.x](https://github.com/EOSIO/eosio.cdt/releases/tag/v1.6.1)
+* [can v1.0.x](https://github.com/canfoundation/CAN/releases/tag/can-v1.0.0)
+* [eosio.cdt v1.7.x](https://github.com/EOSIO/eosio.cdt/releases/tag/v1.7.0)
 
-To build the contracts and the unit tests:
-* First, ensure that your __eosio__ is compiled to the core symbol for the EOSIO blockchain that intend to deploy to.
-* Second, make sure that you have ```sudo make install```ed __eosio__.
-* Third, make sure that you have install CDT version > 1.6.x
-* Then just go to each smart contract folder and following below instruction
+Build the contracts:
+```
+cd cryptobadge
 
-## Prerequisites for Ubuntu & MacOS
-
-* We assume you have installed [Python 3.5](https://www.python.org/downloads/release/python-356/) (or higher).
-* We assume you have installed [pip3 18.1](https://pypi.org/project/pip/) (or higher).
-
-## Install *EOSFactory* on Ubuntu & MacOS
-
-
-Clone *EOSFactory* source code from the repository:
-
-```bash
-git clone https://github.com/tokenika/eosfactory.git
+eosio-cpp -abigen -I include -contract cryptobadge -o cryptobadge.wasm src/cryptobadge.cpp
 ```
 
-Open a bash terminal and navigate to the `eosfactory` folder:
 
-```bash
-cd eosfactory
-```
+Actions:
 
-Then run the `install` script by pointing it to the *EOSIO* source code:
+## badge::regissuer issuer data
+   - Indicates that a particular account wishes to become a issuer
+   - **issuer** account registering to be a badge issuer who has permission to create badge
+   - **data** sha256 of issuer info
 
-```bash
-./install.sh
-```
+## badge::updateissuer issuer data
+   - **issuer** account owner of issuer
+   - **data** sha256 updated issuer info
 
-## Build & Test
+## badge::createbadge issuer version name image_url path description criteria
+   - Create a new badge
+   - **issuer** the account registering as a issuer
+   - **badge_id** the id of badge, should be unique in scope of issuer
+   - **name** the name of badge
+   - **image_url** the url of badge
+   - **path** the path of badge
+   - **description** the description of badge
+   - **criteria** the criteria of badge
 
-Open `build` folder in projects
+## badge::updatebadge issuer badge_id version name image_url path description criteria
+   - **issuer** account has created badge
+   - **badge_id** id of badge
+   - **name** the name of badge
+   - **image_url** the url of badge
+   - **path** the path of badge
+   - **description** the description of badge
+   - **criteria** the criteria of badge
 
-```bash
-cd /cryptobadge
-cd build
-```
+## badge::issuebadge issuer owner idata require_claim
+   - Create a new certification
+   - **issuer** account whose onwer of badge_id
+   - **owner** account whose certification owner
+   - **badge_id** badge id for certification
+   - **badge_revision** the version of badge use for create certificate
+   - **cert_id** certification id for certification, should be unique in scope of owner
+   - **encrypted_data** ceritification encrypted data
+   - **require_claim** true or false. If disabled, upon creation, the certification will be transfered to owner but issuer'S memory will be used until the certification is transferred again).  If enabled issuer will remain the owner, but an offer will be created for the account specified in the owner field to claim the certification using the account's RAM.
 
-To configure your project:
+## badge::claimcert claimer cert_ids
+   - **claimer** account whose onwer of certification
+   - **cert_ids** list certifications to claim. The action will be failed if one cert_ids does not work
+   - Note : claimer must pay RAM for certification if they do this action
 
-```bash
-cmake ..
-```
+## badge::canceloffer issuer cert_ids
+   - **issuer** current certification issuer account
+   - **cert_ids** list certifications to cancel offer
+   - Issuer only cancel offer when the certification still not issue it to owner
+   
+## badge::removecert owner cert_ids memo
+   - Deposits tokens to user REX fund
+   - **owner** current certification owner account
+   - **cert_ids** array of cert_id's to revoke
+   - **memo** memo for revoke action
+   - This action is only available for the certification owner. After executing, the certification will disappear forever, and RAM used for certification will be released.
 
-The output should look like this:
+## badge::attach owner cert_id  data
+   - public certification detail. Action is not mandatory
+   - **owner** certification owner account
+   - **cert_id** id of certificastion when to public on blockchain
+   - **data** json stringify data of certification
+   - Note: once the certification is public, there are no way to remove data on blockchain
 
-```bash
--- Configuring done
--- Generating done
--- Build files have been written to: /mnt/c/Workspaces/EOS/contracts/foo_bar/build
-```
+## badge::detach owner cert_ids
+   - Detach certification detail. Action is not mandatory.
+   - **owner** certification owner account
+   - **cert_ids** list of id certification
+   - The Ram will be release 
 
-To build your project:
+## badge::createlog issuer owner idata cert_id require_claim
+   - Empty action. Used by create action to log cert_id so that third party explorers can easily get new certification ids and other information.
 
-```bash
-make
-```
 
-And finally, after your contract has been successfully built, run unit tests with this command:
 
-```bash
-cd ..
-python3 tests/unittest.py
-```
 
-An example using smart contract by cloes at `example` directory
--- include/cryptobadge.hpp
+++ include/cryptobadge.hpp
@@ -10,271 +10,293 @@
 #include <iterator>
 #include <vector>
 
-
 using namespace eosio;
+using namespace std;
 using std::string;
 
-	class [[eosio::contract("cryptobadge")]] cryptobadge : public contract {
+class[[eosio::contract]] cryptobadge : public contract
+{
 
-	public:
-		//using contract::contract;
-		cryptobadge(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds){}
+public:
+	//using contract::contract;
+	cryptobadge(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
-		/*
+	/*
 		* New issuer registration. issuer is required to create new badge. Markets *may* choose to use information here
 		* to display info about the issuer
 		*
 		* issuer	- issuer account who will create badges;
 		* data		- sha256 string. Recommendations to include: name, url, telephone, desc, image, email, address;
 		*/
-		ACTION regissuer( name issuer, checksum256& data);
+	ACTION regissuer(name issuer, checksum256 & data);
 
-
-		/*
+	/*
 		* issuers info update. Used to update issuer information.
 		*
 		* issuer	- issuers account who will create badges;
 		* data		- sha256 string. Recommendations to include: name, url, telephone, desc, image, email, address;
 		*/
-		ACTION updateissuer( name issuer, checksum256& data);
+	ACTION updateissuer(name issuer, checksum256 & data);
 
-		/*
+	/*
 		* Create a new badge.
 		*
-		* issuer	- badge's issuer, who will able to updated badge's mdata;
-		* category	- badges category;
-		* owner		- badges owner;
-		* idata		- sha256 string with immutable badges data;
-		* mdata		- sha256 string with mutable badges data, can be changed only by issuer;
+		* issuer		- badge's issuer, who will able to updated badge's mdata;
+		* badge_id		- unique id;		
+		* name			- badge name;			
+		* image_url		- image url of badge;
+		* path			- path of badge;
+		* description	- description of badge;
+		* criteria		- criteria of badge;
 		*/
-		ACTION createbadge( name issuer, name owner, checksum256& idata, checksum256& mdata);
+	ACTION createbadge(name issuer, uint64_t badge_id, string name, string image_url, string path, string description, string criteria);
 
-		/*
-		* Update badges mutable data (mdata) field. Action is available only for issuers.
+	/*
+		* Update badges data. Action is available only for issuers.
 		*
-		* issuer	- issuers account;
-		* owner		- current badges owner;
-		* badgeid	- badgeid to update;
-		* mdata		- stringified json with mutable badges data. All mdata will be replaced;
+		* issuer			- issuers account;
+		* badge_id			- badge id to update;
+		* name				- name of badge;
+		* image_url		- image url of badge;
+		* path				- path of badge;
+		* description	- description of badge;
+		* criteria		- criteria of badge;
 		*/
-		ACTION updatebadge( name issuer, name owner, uint64_t badgeid, checksum256& mdata );
+	ACTION updatebadge(name issuer, uint64_t badge_id, string name, string image_url, string path, string description, string criteria);
 
-		/*
+	/*
 		* Create a new certification.
 		*
-		* issuer	- badge owner;
-		* owner		- certification owner;
-		* idata		- sha256 string with immutable certifications data;
-		* requireclaim	- true or false. If disabled, upon creation, the certification will be transfered to owner (but
+		* issuer			- badge owner;
+		* owner				- certification owner;
+		* badge_id			- The badge id which was issued to user;
+		* badge_revision	- The badge revision;		
+		* cert_id			- The certification id;
+		* encrypted_data	- The encrypted certifications data;
+		* encrypted_data	- expired time;
+		* require_claim		- true or false. If disabled, upon creation, the certification will be transfered to owner (but
 		*		  but issuer'S memory will be used until the certification is transferred again).  If enabled,
 		*		  issuer will remain the owner, but an offer will be created for the account specified in
 		*		  the owner field to claim the certification using the account's RAM.
 		*/
-		ACTION createcert( name issuer, name owner, uint64_t badgeid, checksum256& idata, bool requireclaim);
+	ACTION issuebadge( name issuer, name owner, uint64_t badge_id, uint64_t badge_revision, uint64_t cert_id, string& encripted_data, uint64_t expire_at, bool require_claim);
 
-		/*
+	/*
+		* Update certification state to expired.
+		*
+		* cert_id	- certification id;
+		* owner		- the owner of certification;
+		*/
+	ACTION expirecert(name updater, uint64_t cert_id, name owner);
+
+	/*
+		* Revoke certification
+		*
+		* issuer	- issuer of certification;
+		* cert_id	- certification id;
+		* owner		- the owner of certification;
+		*/
+	ACTION revokecert(name issuer, uint64_t cert_id, name owner, string reason);
+
+	/*
 		* Claim the specified certification (assuming it was offered to claimer by the certification owner).
 		*
 		* claimer	- account claiming the badge;
-		* certids	- array of certid's to claim;
+		* cert_ids	- array of cert_id's to claim;
 		*/
-		ACTION claimcert( name claimer, std::vector<uint64_t>& certids);
+	ACTION claimcert(name claimer, std::vector<uint64_t> & cert_ids);
 
-		/*
+	/*
 		* Cancel and remove offer. Available for the certification owner.
 		*
 		* issuer	- current certification issuer account;
-		* certids	- array of certid's to cancel from offer;
+		* cert_ids	- array of cert_id's to cancel from offer;
 		*/
-		ACTION canceloffer( name issuer, std::vector<uint64_t>& certids);
+	ACTION canceloffer(name issuer, std::vector<uint64_t> & cert_ids);
 
-
-		/*
-		* Remove certification {{certid}}. This action is only available for the certification owner. After executing, the
+	/*
+		* Remove certification {{cert_id}}. This action is only available for the certification owner. After executing, the
 		* certification will disappear forever, and RAM used for certification will be released.
 		*
 		* owner		- current certification owner account;
-		* certids	- array of certid's to remove;
+		* cert_ids	- array of cert_id's to remove;
 		* memo		- memo for remove action;
 		*/
-		ACTION removecert( name owner, std::vector<uint64_t>& certids, string memo );
+	ACTION removecert(name owner, std::vector<uint64_t> & cert_ids, string memo);
 
-  
-		/*
-		* Attach certification detail. Action is not mandatory.  owner *may* choose to public information here
-		* to display info
-		*
-		* owner	- account who is certification owner;
-		* certid - certid's to attach
-		* data		- stringified json. Recommendations to include: issuedOn, evidences, narratives, expire, description;
-		*/
-		ACTION attach( name owner, uint64_t certid, string data);
-	
-	
-		/*
-		* detach certification detail. Action is not mandatory.  owner *may* choose to remove their information
-		*
-		* owner	- account who is certification owner;
-		* certids - array of certid's to detach
-		* data		- stringified json. Recommendations to include: issuedOn, evidences, narratives, expire, description;
-		*/	
-		ACTION detach( name owner, std::vector<uint64_t>& certids);
-
-
-		/*
-		* Empty action. Used by create action to log certid so that third party explorers can
+	/*
+		* Empty action. Used by create action to log cert_id so that third party explorers can
 		* easily get new certification ids and other information.
 		*/
-		ACTION createlog( name issuer, name owner, checksum256& idata, uint64_t certid, bool requireclaim);
+	ACTION createlog(name issuer, name owner, checksum256 & idata, uint64_t cert_id, bool require_claim);
 
+private:
+	enum CertificationState
+	{
+		CERTIFIED = 0,
+		EXPIRED,
+		REVOKED
+	};
 
-	private:
+	uint64_t getid(uint64_t gindex);
 
-		uint64_t getid(uint64_t gindex);	
+	template <typename... Args>
+	void sendEvent(name issuer, name rampayer, name seaction, const std::tuple<Args...> &tup);
 
-		bool compareHash(const checksum256& current_hash, const checksum256&  input_hash);
-
-		template<typename... Args>
-		void sendEvent(name issuer, name rampayer, name seaction, const std::tuple<Args...> &tup);
-
-		/*
+	/*
 		* issuers table. Can be used by badge markets, badge explorers, or wallets for correct badge
 		* data presentation.
 		* Scope: self
 		*/
-    TABLE cissuer
-    {
-			name						issuer;
-			checksum256			data;
+	TABLE cissuer
+	{
+		name issuer;
+		checksum256 data;
 
-			auto primary_key() const {
-				return issuer.value;
-			}
-    };
-    typedef multi_index<"issuers"_n, cissuer> issuers;
+		auto primary_key() const
+		{
+			return issuer.value;
+		}
+	};
+	typedef multi_index<"v1.issuers"_n, cissuer> issuers;
 
+	/*
+	* Badges table which stores information about simple badges.
+	* Scope: badge owner
+	*/
+	TABLE cbadge
+	{
+		uint64_t badge_id;
+		name issuer;
+		uint64_t version = 1;
 
-		/*
-		* Badges table which stores information about simple badges.
-		* Scope: badge owner
-		*/
-	   TABLE cbadge
-    {
-			uint64_t                badgeid;
-			name                    issuer;
-			name                    owner;
-			checksum256        idata;
-			checksum256        mdata;
+		auto primary_key() const { return badge_id; }
+		uint64_t by_issuer() const { return issuer.value; }
+	};
 
-			auto primary_key() const {
-				return badgeid;
-			}
-			uint64_t by_issuer() const {
-				return issuer.value;
-			}
-    };
-  	typedef eosio::multi_index< "cbadges"_n, cbadge,
-			eosio::indexed_by< "issuer"_n, eosio::const_mem_fun<cbadge, uint64_t, &cbadge::by_issuer> >
-			> cbadges;
-			
+	typedef eosio::multi_index<"v1.badges"_n, cbadge,
+							   eosio::indexed_by<"issuer"_n, eosio::const_mem_fun<cbadge, uint64_t, &cbadge::by_issuer>>>
+		cbadges;
 
-		/*
+	/*
 		* Certification table which stores information about  certifications.
 		* Scope: certification owner
 		*/
-		TABLE ccert
+	TABLE ccert
+	{
+		uint64_t id;
+		uint64_t badge_id;
+		uint64_t badge_revision;
+		name owner;
+		uint64_t state;
+		uint64_t expire_at;
+		string encripted_data;
+
+		auto primary_key() const
 		{
-			uint64_t                id;
-			name                    owner;
-			name                    issuer;
-			uint64_t                badgeid;
-			checksum256        idata;
+			return id;
+		}
+		uint64_t by_badge_id() const
+		{
+			return badge_id;
+		}
+		uint64_t by_owner() const
+		{
+			return owner.value;
+		}
+	};
 
-			auto primary_key() const {
-				return id;
-			}
-			uint64_t by_issuer() const {
-				return issuer.value;
-			}
+	typedef eosio::multi_index<"v1.certs"_n, ccert,
+							   eosio::indexed_by<"badgeid"_n, eosio::const_mem_fun<ccert, uint64_t, &ccert::by_badge_id>>,
+							   eosio::indexed_by<"owner"_n, eosio::const_mem_fun<ccert, uint64_t, &ccert::by_owner>>>
+		ccerts;
 
-		};
-
-		typedef eosio::multi_index< "ccerts"_n, ccert,
-			eosio::indexed_by< "issuer"_n, eosio::const_mem_fun<ccert, uint64_t, &ccert::by_issuer> >
-			> ccerts;
-
-		/*
+	/*
 		* Certification table which stores information about  certifications.
 		* Scope: certification owner
 		*/
-		TABLE ccertinfo
+	TABLE ccertinfo
+	{
+		uint64_t cert_id;
+		string cert_content;
+
+		auto primary_key() const
 		{
-			uint64_t      cert_id;
-			string        cert_content;
+			return cert_id;
+		}
+	};
 
-			auto primary_key() const {
-				return cert_id;
-			}
+	typedef eosio::multi_index<"v1.certinfo"_n, ccertinfo> ccertinfos;
 
-		};
-
-		typedef eosio::multi_index< "ccertinfos"_n, ccertinfo> ccertinfos;
-
-		/*
+	/*
 		* Offers table keeps records of open offers of certifications (ie. certifications waiting to be claimed by their
 		* intendend recipients.
 		*
 		* Scope: self
 		*/
-		TABLE coffer
+	TABLE coffer
+	{
+		uint64_t cert_id;
+		name owner;
+		name offered_to;
+		time_point date;
+
+		auto primary_key() const
 		{
-			uint64_t		certid;
-			name			owner;
-			name			offeredto;
-			time_point		cdate;
+			return cert_id;
+		}
+		uint64_t by_owner() const
+		{
+			return owner.value;
+		}
+		uint64_t by_offered_to() const
+		{
+			return offered_to.value;
+		}
+	};
 
-			auto primary_key() const {
-				return certid;
-			}
-			uint64_t by_owner() const {
-				return owner.value;
-			}
-			uint64_t by_offeredto() const {
-				return offeredto.value;
-			}
-		};
+	typedef eosio::multi_index<"v1.offers"_n, coffer,
+							   eosio::indexed_by<"owner"_n, eosio::const_mem_fun<coffer, uint64_t, &coffer::by_owner>>,
+							   eosio::indexed_by<"offeredto"_n, eosio::const_mem_fun<coffer, uint64_t, &coffer::by_offered_to>>>
+		offers;
 
-		typedef eosio::multi_index< "offers"_n, coffer,
-			eosio::indexed_by< "owner"_n, eosio::const_mem_fun<coffer, uint64_t, &coffer::by_owner> >,
-			eosio::indexed_by< "offeredto"_n, eosio::const_mem_fun<coffer, uint64_t, &coffer::by_offeredto> >
-			> offers;
-
-
-
-		/*
-		* global singelton table, used for certid building
+	/*
+		* global singelton table, used for cert_id building
 		* Scope: self
 		*/
-		TABLE global{
-		  
-			global(){}
-			uint64_t deferid	= 100000000000000;
-			uint64_t certid		= 1000000;
-			uint64_t badgeid	= 0000000;
+	TABLE global
+	{
 
-			EOSLIB_SERIALIZE( global, (deferid)(certid)(badgeid) )
-		};
+		global() {}
+		uint64_t defer_id = 100000000000000;
+		uint64_t cert_id = 1000000;
+		uint64_t badge_id = 0000000;
 
-		typedef eosio::singleton< "global"_n, global> conf;
-		global _cstate;
+		EOSLIB_SERIALIZE(global, (defer_id)(cert_id)(badge_id))
+	};
 
-		enum gindex : uint8_t
-		{
-			DEFER = 0,
-			CERT = 1,
-			BADGE = 2,
-		};
+	typedef eosio::singleton<"v1.global"_n, global> conf;
+	global _cstate;
 
+	enum gindex : uint8_t
+	{
+		DEFER = 0,
+		CERT = 1,
+		BADGE = 2,
+	};
 
-	
+	//refer govenance design
+	TABLE communityf
+	{
+		name community_account;
+		name creator;
+		string community_name;
+		vector<uint64_t> member_badge;
+		string community_url;
+		string description;
+
+		uint64_t primary_key() const { return community_account.value; }
+	};
+
+	typedef eosio::multi_index<"community"_n, communityf> community_table;
 };
